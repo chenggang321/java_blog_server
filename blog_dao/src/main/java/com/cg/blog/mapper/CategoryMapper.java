2dcecceb16c0d5e91d5567dbb03a02415c6a0847
chenggang <chenggang@hua-yong.com>
2020-04-22 22:04:04 +0800

    pojo mapper 添加

---
+++ blog_dao/src/main/java/com/cg/blog/mapper/CategoryMapper.java
@@ -0,0 +1,7 @@
+package com.cg.blog.mapper;
+
+import com.cg.blog.pojo.Category;
+import tk.mybatis.mapper.common.Mapper;
+
+public interface CategoryMapper extends Mapper<Category> {
+}
---
+++ blog_dao/src/main/java/com/cg/blog/mapper/ContentMapper.java
@@ -0,0 +1,7 @@
+package com.cg.blog.mapper;
+
+import com.cg.blog.pojo.Content;
+import tk.mybatis.mapper.common.Mapper;
+
+public interface ContentMapper extends Mapper<Content> {
+}
