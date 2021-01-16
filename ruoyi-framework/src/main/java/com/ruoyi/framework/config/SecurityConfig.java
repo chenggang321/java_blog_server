40f88509ee69103787e245aba8f7cd7f4b72448b
chenggang <1129137164@qq.com>
2021-01-17 00:45:35 +0800

    权限验证调整

--- ruoyi-framework/src/main/java/com/ruoyi/framework/config/SecurityConfig.java
+++ ruoyi-framework/src/main/java/com/ruoyi/framework/config/SecurityConfig.java
@@ -102,9 +102,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter
                 // 对于登录login 验证码captchaImage 允许匿名访问
                 .antMatchers(
                         "/login",
-                        "/captchaImage",
-                        "/blog/category/list",
-                        "/blog/content/list"
+                        "/captchaImage"
                 ).anonymous()
                 .antMatchers(
                         HttpMethod.GET,
@@ -121,6 +119,11 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter
                 .antMatchers("/webjars/**").anonymous()
                 .antMatchers("/*/api-docs").anonymous()
                 .antMatchers("/druid/**").anonymous()
+                .antMatchers(
+                        HttpMethod.GET,
+                        "/blog/category/list",
+                        "/blog/content/list"
+                ).permitAll()
                 // 除上面外的所有请求全部需要鉴权认证
                 .anyRequest().authenticated()
                 .and()
